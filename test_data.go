b58c11551755543e3fc101f22961effee543108f
Rafael Fonseca <r4f4rfs@gmail.com>
2020-02-15 22:24:07 +0100

    Add some test data shared by all tests

---
+++ test_data.go
@@ -0,0 +1,13 @@
+package main
+
+var tilesSolved = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0}
+var tilesUnsolved = []int{1, 2, 3, 4, 0, 5, 6, 7, 8, 10, 11, 9, 12, 13, 14, 15}
+var tilesOrdered = []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+var tilesAlmost = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 0, 15}
+var tilesAlldirs = []int{1, 2, 3, 4, 5, 0, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+var tilesImpossible = []int{1, 2, 3, 0, 4, 5, 6, 7, 8, 10, 11, 9, 12, 13, 14, 15}
+var tilesMissing = []int{1, 2, 3, 0, 4}
+var tilesWrong = []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 19, 10, 11, 12, 13, 14, 16}
+var tilesMissingEmtpy = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
+var tilesRepeated = []int{0, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}
+var tilesExcess = []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
